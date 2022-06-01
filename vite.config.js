3a239f4b0e19947062a99aa6648cfb50f59ebfec
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-06-01 01:30:45 -0500

    change build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }]
+  },
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
